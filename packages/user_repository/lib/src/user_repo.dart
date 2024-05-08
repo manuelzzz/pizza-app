c04587e31e8301694504b7bdb03d1747f2e0db9f
manuelzzz <manuelss.23.bil@gmail.com>
2024-05-08 11:36:12 -0300

    fix: correction in parameters and classes

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -3,9 +3,9 @@ import 'package:user_repository/src/models/models.dart';
 abstract class UserRepo {
   Stream<MyUser?> get user;
 
-  Future<MyUser> signUp(MyUser user, String password);
+  Future<MyUser> signUp(MyUser myUser, String password);
 
-  Future<void> signIn(MyUser user, String password);
+  Future<void> signIn(String email, String password);
 
   Future<void> setUserData(MyUser user);
 
